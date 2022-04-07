d60c6027720017392e9a9f1876be9318e003008a
tejakattenborn <38572828+tejakattenborn@users.noreply.github.com>
2022-04-08 00:07:22 +0200

    Add files via upload

---
+++ code_run_AngleCam/cnn_reqs.R
@@ -0,0 +1,118 @@
+
+
+# -------- Description
+# This script trains CNN models using the pairs of input imagery and leaf angle distributions.
+# The training includes a data augmentation on both the predictors (imagery) and the response (leaf angle distributions).
+# The augmentation of the imagery includes color modifications, geometric operations and selecting different parts of the imagery
+# The augmentation of the leaf angle distribution is based on the variants of the fitted beta distributions (one variant is randmoly selected during the training process)
+# The data is loaded on the fly from harddist (tfdataset input pipeline). The input pipeline will act differently on the training and the test data (no augmentation on test data).
+# different (pretrained) backbones may be used
+# all model objects and are written to checkpoints and can restored from there after training (per default the best model as determined by the test dataset will be loaded).
+
+
+# image settings
+xres = 512L # max 1280 with brinno
+yres = 512L #  max 690 with brinno
+no_bands = 3L # RGB
+angle_resolution = 45L-2L # intervals of the leaf angle distributions
+
+# hyperparameters
+batch_size <- 10L # 8 for 256 * 256
+num_epochs <- 200L
+
+
+# tfdatasets input pipeline -----------------------------------------------
+
+create_dataset <- function(data,
+                           train, # logical. TRUE for augmentation of training data
+                           batch, # numeric. multiplied by number of available gpus since batches will be split between gpus
+                           epochs,
+                           aug_rad,
+                           aug_geo,
+                           shuffle, # logical. default TRUE, set FALSE for test data
+                           dataset_size){ # numeric. number of samples per epoch the model will be trained on
+  # the data will be shuffled in each epoch
+  if(shuffle){
+    dataset = data %>%
+      tensor_slices_dataset() %>%
+      dataset_shuffle(buffer_size = length(data$img), reshuffle_each_iteration = TRUE)
+  } else {
+    dataset = data %>%
+      tensor_slices_dataset() 
+  }
+  
+  # Apply data augmentation for training data
+  if(train){
+    dataset = dataset %>%
+      dataset_map(~.x %>% purrr::list_modify( # read files and decode png
+        #img = tf$image$decode_png(tf$io$read_file(.x$img), channels = no_bands)
+        img = tf$image$decode_jpeg(tf$io$read_file(.x$img), channels = no_bands, try_recover_truncated = TRUE, acceptable_fraction=0.3) %>%
+          tf$image$convert_image_dtype(dtype = tf$float32), #%>%
+        ref = .x$ref[sample(2:ref_min_no, 1),] # sample from beta distributions with random errors
+      ), num_parallel_calls = NULL)# %>%
+  }else{
+    dataset = dataset %>%
+      dataset_map(~.x %>% purrr::list_modify( # read files and decode png
+        #img = tf$image$decode_png(tf$io$read_file(.x$img), channels = no_bands)
+        img = tf$image$decode_jpeg(tf$io$read_file(.x$img), channels = no_bands, try_recover_truncated = TRUE, acceptable_fraction=0.3) %>%
+          tf$image$convert_image_dtype(dtype = tf$float32), #%>%
+        ref = .x$ref[1,] # sample from original beta distributions
+      ), num_parallel_calls = NULL)# %>%
+  }
+  
+  # geometric modifications
+  if(aug_geo) {
+    dataset = dataset %>%
+      dataset_map(~.x %>% purrr::list_modify( # randomly flip left/right
+        img = tf$image$random_flip_left_right(.x$img, seed = 1L) %>%
+          tf$image$random_crop(size = c(552L, 1024L, 3L)) %>% # will crop within image, original height = 690 *0.8, width = 1280*0,8
+          tf$keras$preprocessing$image$smart_resize(size=c(xres, yres))
+      ))
+  }else{
+    dataset = dataset %>%
+      dataset_map(~.x %>% purrr::list_modify( # randomly flip left/right
+        img = tf$keras$preprocessing$image$smart_resize(.x$img, size=c(xres, yres))
+      ))
+  }
+  # radiometric modifications
+  if(aug_rad) {
+    dataset = dataset %>%
+      dataset_map(~.x %>% purrr::list_modify( # randomly assign brightness, contrast and saturation to images
+        img = tf$image$random_brightness(.x$img, max_delta = 0.1, seed = 1L) %>% 
+          tf$image$random_contrast(lower = 0.8, upper = 1.2, seed = 2L) %>%
+          tf$image$random_saturation(lower = 0.8, upper = 1.2, seed = 3L) %>% # requires 3 chnl -> with useDSM chnl = 4
+          tf$clip_by_value(0, 1) # clip the values into [0,1] range.
+      ))
+  }
+  if(train) {
+    dataset = dataset %>%
+      dataset_repeat(count = epochs) 
+  }
+  dataset = dataset %>% dataset_batch(batch, drop_remainder = TRUE) %>%
+    dataset_map(unname) %>%
+    dataset_prefetch(buffer_size = tf$data$experimental$AUTOTUNE)
+  #dataset_prefetch_to_device("/gpu:0", buffer_size = tf$data$experimental$AUTOTUNE)
+}
+
+
+
+# Definition of the CNN structure -----------------------------------------------
+
+base_model <- application_efficientnet_b7(
+  input_shape = c(xres, yres, no_bands),
+  include_top = FALSE,
+  drop_connect_rate=0.1, # 0.2 is default
+  #include_preprocessing=True,
+  pooling = NULL
+)
+
+# custom layers v2
+predictions <- base_model$output %>%
+  layer_global_average_pooling_2d() %>%
+  layer_dense(units = angle_resolution, activation = 'linear')
+
+# merge base model and custom layers
+model <- keras_model(inputs = base_model$input, outputs = predictions)
+
+# compile
+model %>% compile(optimizer = optimizer_adam(learning_rate = 0.0001), loss = 'mse') # mse or mae?
---
+++ code_run_AngleCam/run_AngleCam.R
@@ -0,0 +1,77 @@
+
+### ----------------------------------------------------------------------------------------------
+### ---- AngleCam --------------------------------------------------------------------------------
+### ----------------------------------------------------------------------------------------------
+
+# This is the alpha version of AngleCam (2022-04-07)
+# AngleCam estimates leaf angle distributions from conventional RGB photographs
+# for details see: https://github.com/tejakattenborn/AngleCAM
+# AngleCam can be applied on horizontally acquired imagery with a minumum image size of 512x512 px.
+# Input imagery can be of different size; the pipeline will resample them on the fly to 512x512 px.
+# Find below two examples for visualizing estimates as leaf angle distribution or average leaf angle
+
+# -------TensorFlow------------------------------------------------------------------------------
+
+# Sketch and some info on installing TensorFlow (sometimes tricky):
+# devtools::install_github("rstudio/tensorflow")
+# install_tensorflow(version = "2.5.0")
+# > official github repository: https://github.com/rstudio/tensorflow
+# > General info on installing Tensorflow: https://tensorflow.rstudio.com/installation/
+# > running TensorFlow with GPU support is recommended but not mandatory (CPU also works fine, but slower)
+# > Hints for TensorFlow with GPU support: https://tensorflow.rstudio.com/installation/gpu/local_gpu/
+# > hello <- tf$constant("Hello"); print(hello) # check TensorFlow installation
+
+# ------Dependencies--------------------------------------------------------------------------------
+
+library(reticulate)
+reticulate::use_condaenv("rtf2", required = TRUE)
+#reticulate::conda_binary() # "/net/home/tkattenborn/.local/share/r-miniconda/bin/conda"
+library(tensorflow)
+library(keras)
+library(dplyr)
+library(tfdatasets)
+
+setwd("/net/home/tkattenborn/data_brinno/data_2021_lak_brinno/1_AngleCam_application")
+
+### GPU settings (only needed if tensorflow is run on GPU, recommended but not mandatory; CPU also works)
+gpu <- tf$config$experimental$get_visible_devices('GPU')[[1]] # initialize GPU
+tf$config$experimental$set_memory_growth(device = gpu, enable = TRUE) # adjust GPU RAM usage according to actual demand
+
+### compile the model and initialize the tfdatasets pipeline
+source("cnn_reqs.R")
+# load model weights
+model <- load_model_hdf5("AngleCam_efficientnet_b7_07-04-2022.hdf5", compile = FALSE)
+
+# -------AngleCam prediction----------------------------------------------------------------------
+
+### load image data and create tfdataset (the imagery will not be loaded to RAM but processed on the fly; there are, hence, no limits regarding dataset size)
+imagepaths <- list.files(path = "example_dataset_timeseries_tilia_cordata", full.names = T, pattern = ".png", recursive = F); paste0(length(imagepaths), " images found.")
+imgdataset <- create_dataset(tibble(img = imagepaths),  batch = 1, epochs = 1, shuffle = F, train = F, aug_geo = F, aug_rad = F)
+
+### apply the model to the imagery and write output to file
+pred_imgdataset <- model %>% predict(imgdataset)
+pred_imgdataset <- pred_imgdataset/10
+rownames(pred_imgdataset) <- basename(imagepaths)
+write.csv(pred_imgdataset, file = "AngleCam_predictions_example_dataset_timeseries_tilia_cordata.csv", row.names = T)
+
+#--------simple visulization--------------------------------------------------------------------
+
+### plot leaf angle distributions over time
+for(i in 1:nrow(pred_imgdataset)){
+  if (i%%10 == 0) {
+    #lines(seq(0,90, length.out = 43), pred_imgdataset[i,])
+    plot(seq(0,90, length.out = 43), pred_imgdataset[i,], ylim=c(0,0.07), xlim=c(0,90), type="l", main = substr(rownames(pred_imgdataset)[i], 15, 33), ylab = "density", xlab = "leaf angle [deg]")
+    Sys.sleep(0.6)
+  }
+}
+
+### plot average leaf angles over time
+# derive average leaf angle from leaf angle distributions
+mean_angle = function(pred){sum(pred*seq(0,90, length.out = ncol(pred_imgdataset)))}
+avg_angle <- apply(pred_imgdataset, 1, mean_angle)
+# derive date from filename
+dates = as.POSIXct(substr(rownames(pred_imgdataset), 15, 33), tz = "Europe/Berlin", format = "%Y-%m-%d_%H-%M-%OS")
+# plot leaf angles for one day
+plot(dates, avg_angle, ylab = "Average leaf angle [deg]", xlab = "time (CEST)")
+
+
